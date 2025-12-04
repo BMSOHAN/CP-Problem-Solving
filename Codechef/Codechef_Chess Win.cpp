545bfb05e9ed9d2a910c3ccf0e8c4f738781a49e
BMSOHAN <fazlaelahisohan@gmail.com>
2025-12-04 22:13:09 +0600

    Create Codechef_Chess Win.cpp

---
+++ Codechef/Codechef_Chess Win.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+
+int main(){
+    ios::sync_with_stdio(false);
+    int a, b;
+    cin a >> b;
+    cout << abs(b - a) + 1 << "\n";
+    return 0;
+}
